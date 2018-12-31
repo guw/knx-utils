37b3ea4482c9d930ddf00cb52ad82c2e6dc00f64
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2018-12-31 09:30:27 +0100

    Prototype parser for reading KNX project files from ETS
    Signed-off-by: Gunnar Wagenknecht <gunnar@wagenknecht.org>

---
+++ knx-openhab-utils/src/main/java/io/guw/knxopenhabutils/App.java
@@ -0,0 +1,29 @@
+package io.guw.knxopenhabutils;
+
+import java.io.File;
+import java.util.concurrent.Callable;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Parameters;
+
+/**
+ * Converts a knxproj file to a set of files for openHAB.
+ */
+public class App implements Callable<Void> {
+
+	public static void main(String[] args) {
+		CommandLine.call(new App(), args);
+	}
+
+	@Parameters(index = "0", description = "The .knxproj file to convert.")
+	private File knxProjFile;
+
+	@Override
+	public Void call() throws Exception {
+
+		KnxProjectFile knxProjectFile = new KnxProjectFile(knxProjFile);
+		knxProjectFile.open();
+
+		return null;
+	}
+}
---
+++ knx-openhab-utils/src/test/java/io/guw/knxopenhabutils/AppTest.java
@@ -0,0 +1,12 @@
+package io.guw.knxopenhabutils;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+	@Test
+	public void testApp() {
+	}
+}
